6474274657bbce29d7db80998b0b0ffe58a3358b
Semi Invader <=>
2023-03-08 18:58:14 -0600

    in order to use bscrpc, need a block number close to actual but not that far away

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const privateKeys = process.env.PRIVATE_KEY?.split(" ") ;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
     hardhat: {
       chainId: 1337,
-    },
-    testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-    },
-    mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
+      forking: {
+        url: "https://bscrpc.com",
+        blockNumber: 26301200,
+      },
     },
   }
 };
